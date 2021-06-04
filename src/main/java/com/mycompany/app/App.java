b8f5ab3eed22658f12a5d7fc5a3a9bac2aa25e32
Komal0912 <komalricha1997@gmail.com>
2021-06-04 19:44:15 +0530

    Added new version string

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -6,16 +6,23 @@ package com.mycompany.app;
 public class App
 {
 
-    private final String message = "Hello World!";
+    private final String message = "Hello World!" ;
+
+    private final String message1="Changes made in the string!!!";
+
 
     public App() {}
 
     public static void main(String[] args) {
         System.out.println(new App().getMessage());
+        System.out.println(new App().getMessage1());
     }
 
     private final String getMessage() {
         return message;
+    }
+     private final String getMessage1() {
+        return message1;
     }
 
 }
