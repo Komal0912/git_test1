0eab7b0f8195d71200529e4e5330865d510347d0
Komal0912 <komalricha1997@gmail.com>
2021-06-08 12:48:13 +0530

    Trial Code to test changes in jenkins!

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,12 +10,15 @@ public class App
 
     private final String message1="Changes made in the string!!!";
 
+    private final String msg="Editing code to test in jenkins!";
+
 
     public App() {}
 
     public static void main(String[] args) {
         System.out.println(new App().getMessage());
         System.out.println(new App().getMessage1());
+        System.out.println(new App().getMsg());
     }
 
     private final String getMessage() {
@@ -23,6 +26,10 @@ public class App
     }
      private final String getMessage1() {
         return message1;
+    }
+
+    private final String getMsg(){
+        return msg;
     }
 
 }
